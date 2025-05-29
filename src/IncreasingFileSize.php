86ba34fc98d0886f5d9f53ceca9bd7163c827cf0
Rasmus Nielsen <rasmus@easify.dk>
2025-05-29 14:55:56 +0200

    Feature/laravel backup 9 (#11)
    * Upgrade to Laravel backup 9
    
    * Apply fixes from StyleCI (#10)
    
    [ci skip] [skip ci]

--- src/IncreasingFileSize.php
+++ src/IncreasingFileSize.php
@@ -26,7 +26,7 @@ class IncreasingFileSize extends HealthCheck
     /**
      * @param  BackupDestination  $backupDestination
      */
-    public function checkHealth(BackupDestination $backupDestination)
+    public function checkHealth(BackupDestination $backupDestination): void
     {
         if ($backupDestination->backups()->count() < 2) {
             return;
@@ -53,7 +53,7 @@ class IncreasingFileSize extends HealthCheck
     }
 
     /**
-     * @param $value
+     * @param  $value
      * @return float|int
      */
     protected function parseValue($value)
